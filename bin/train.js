4dce660f1c944f250daa004aa8707e78f0683b2d
Phillip Rosen <phill.rosen@gmail.com>
2012-10-15 17:45:57 -0400

    output total $allocated

--- bin/train.js
+++ bin/train.js
@@ -1,27 +1,29 @@
 var fs = require('fs'),
-    async = require('async'),
     Analyzer = require('./Analysis').Analyzer;
 
-var i = 0;
-var rows = [];
 
 var file = fs.readFileSync(__dirname + '/../data/data.csv', 'utf8')
-var rows = file.split('\n').slice(1, 500);
+var rows = file.split('\n');
+var lastRow = rows.slice(rows.length,rows.length+1);
+rows = rows.slice(1, rows.length);
 var budget = 10000;
+
+//Build the training set
 var trainingSet = [];
 for (var j = 0; j < rows.length; j++) {
     trainingSet.push(getInputRow(rows[j].split(',').slice(1, 101),budget));
 }
 
+//Test it based on rows.length-1 entries
 var analyzer = new Analyzer();
 analyzer.train(trainingSet, {log:true, logPeriod:100, iterations:10});
 
-//We will use the last row as the input.
-var lastRow = rows.slice(500,501);
 
-//Test gives us the weightings
+//Test gives us the weightings for the lastRow based on the training set
 var allocationWeights = analyzer.test(getInputRow(lastRow).input,budget);
 var allocations = analyzer.caclulateAllocation(allocationWeights,budget);
+
+//Output the allocated amounts
 console.log(allocations);
 
 var allocated=0;
@@ -30,6 +32,11 @@ for (var a in allocations){
 }
 console.log('Allocated: '+allocated);
 
+/**
+ * Builds an input row based on the budget.
+ * @param row
+ * @param budget
+ */
 function getInputRow(row,budget) {
     var clicksSum = 0;
     var bestClicks = -1;
@@ -42,7 +49,9 @@ function getInputRow(row,budget) {
         }
     }
 
+    //Input object
     var input = {};
+    //Contains the best ads indexes that will get the most budget allocated
     var alloc = [];
     var output = {};
     var bestWeighted = -1;
@@ -59,12 +68,9 @@ function getInputRow(row,budget) {
         }
     }
 
-    var allocated=0;
-
     //Add the allocated % of the budget to the output. The max value gets %100 of the budget, if multiple max vals, split it.
     for (var j = 0; j < row.length; j++) {
         output['a'+j]=alloc.indexOf(j)>-1 ? ((1 / alloc.length)*budget)/budget : 0;
-        allocated+=output['a'+j];
     }
 
     return {input:input, output:output};
